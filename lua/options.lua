6593891ac428c01b9ff4b2a5c4411519b4824c90
mezeirobert0 <antoniu.mezei@stud.ubbcluj.ro>
2024-08-20 12:02:50 +0300

    Update nerd font variable, change cursor to block in insert mode, make neo-tree plugin available

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,9 @@
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- make cursor in insert mode a block
+vim.opt.guicursor = ''
+
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
